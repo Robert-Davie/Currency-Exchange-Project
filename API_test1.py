7275dabc95a3deb7649bc2186945080e10548f68
Brian Gurdas <briangurdas@brians-mbp.home>
2025-02-22 15:55:27 +0000

    Add target value for gbp and error checking for API connection

--- API_test1.py
+++ API_test1.py
@@ -5,6 +5,7 @@ Desc: This is just a test!!
 
 import requests
 import json
+import datetime
 
 print("Start of API Script")
 
@@ -13,8 +14,13 @@ currency_codes_json = requests.get("https://cdn.jsdelivr.net/npm/@fawazahmed0/cu
 print(f"Result 1: {result_1.json()}")
 '''
 
-currency_value_API = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json"
+
+currency_value_API = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/gbp.jso"
 all_currencies_response = requests.get(currency_value_API)
+
+if all_currencies_response.status_code != 200:
+    print(f"Non 200 code")
+    exit(2)
 
 '''
 Printing all currency values.
@@ -30,14 +36,44 @@ else:
 Printing specific currency value from JSON result.
 '''
 target_currency = "gbp"
-if all_currencies_response.status_code == 200:
-    data = all_currencies_response.json()
-    # 2 layers
-    target_value = data.get("eur").get(target_currency)
+# if all_currencies_response.status_code == 200:
+#     data = all_currencies_response.json()
+#     # 2 layers
+#     target_value = data.get("eur").get(target_currency)
     
-    if target_value is not None:
-        print(f"Value of {target_currency}: {target_value}")
-    else:
-        print(f"Key {target_currency} not found in JSON.")
-else:
-    print("Error fetching data from API.")
+#     if target_value is not None:
+#         print(f"Value of {target_currency}: {target_value}")
+#     else:
+#         print(f"Key {target_currency} not found in JSON.")
+# else:
+#     print("Error fetching data from API.")
+
+target_currencies = [
+    "gbp", 
+    "eur",
+    "usd",
+    "jpy",
+    "inr",
+    "rub"
+    
+]
+
+exchange_rates = {}
+
+exchange_rates["Date_from_API"] = data.get("date")
+now = datetime.datetime.now()
+formatted_time = now.strftime("%Y-%m-%d-%H:%M")
+exchange_rates["Current_Time"] = formatted_time
+
+for currency in target_currencies:
+    current_rate = data.get("gbp").get(currency)
+    current_rate = round(current_rate, 3) 
+    exchange_rates[currency] = current_rate
+    #print(f"Currency: {currency} Current Rate: {current_rate}")
+print(exchange_rates)
+
+
+   
+
+
+
